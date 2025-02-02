e18972d72d4683051b79fcef495cd5802c201168
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-02-02 01:10:56 -0500

    Write tests for the 'Error500' page.

---
+++ extension/src/pages/Error500/Error500.test.tsx
@@ -0,0 +1,25 @@
+import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+
+import { Error500 } from "./Error500";
+
+describe("Error500 component", () => {
+  it("renders message, error status, and link to the home page", () => {
+    const message = "Oops. Try again later.";
+    const { asFragment } = render(
+      <MemoryRouter>
+        <Error500 message={message} />
+      </MemoryRouter>
+    );
+
+    const status = screen.getByText("500");
+    const errorMessage = screen.getByText(`Internal Server Error: ${message}`);
+    const link = screen.getByRole("link", {
+      name: "Go back to the home page.",
+    });
+    expect(status).toBeInTheDocument();
+    expect(errorMessage).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
--- extension/src/pages/Error500/Error500.tsx
+++ extension/src/pages/Error500/Error500.tsx
@@ -1,5 +1,7 @@
 import CSSModules from "react-css-modules";
 import { Link } from "react-router-dom";
+
+import robotImage from "../../assets/images/robot.png";
 import styles from "./Error500.module.css";
 
 interface Props {
@@ -11,23 +13,17 @@ export const Error500 = CSSModules(
     return (
       <div styleName="error">
         <div styleName="error__container">
-          <img
-            styleName="error__image"
-            src={"/images/robot.png"}
-            alt="sad robot looking down"
-          />
+          <img styleName="error__image" src={robotImage} alt="" />
           <h2 styleName="error__number">500</h2>
           <hr styleName="error__break" />
           <p styleName="error__message">Internal Server Error: {message}</p>
           <Link styleName="error__link" to="/">
-            Go back to the home page
+            Go back to the home page.
           </Link>
         </div>
       </div>
     );
   },
   styles,
-  { allowMultiple: true, handleNotFoundStyleName: "ignore" }
+  { allowMultiple: true, handleNotFoundStyleName: "log" }
 );
-
-export default Error500;
