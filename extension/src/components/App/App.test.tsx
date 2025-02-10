c79b25d8de4b61bf8dcf58ad4b18d6cea548a740
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-02-10 01:01:59 -0500

    Add the 'Navigation' and 'Footer' components to the root component.

--- extension/src/components/App/App.test.tsx
+++ extension/src/components/App/App.test.tsx
@@ -1,11 +1,41 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { App } from "./App";
 
+vi.mock("../../layouts/Footer", () => {
+  return {
+    Footer: function () {
+      return <div data-testid="footer"></div>;
+    },
+  };
+});
+
+vi.mock("../../layouts/Navigation", () => {
+  return {
+    Navigation: function () {
+      return <div data-testid="navigation"></div>;
+    },
+  };
+});
+
+vi.mock("react-router-dom", () => {
+  return {
+    Outlet: function () {
+      return <div data-testid="outlet"></div>;
+    },
+  };
+});
+
 describe("App component", () => {
-  it("renders main element", () => {
+  it("renders main element and footer", () => {
     const { asFragment } = render(<App />);
 
+    const footer = screen.getByTestId("footer");
+    const navigation = screen.getByTestId("navigation");
+    const outlet = screen.getByTestId("outlet");
+    expect(footer).toBeInTheDocument();
+    expect(navigation).toBeInTheDocument();
+    expect(outlet).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- extension/src/components/App/App.tsx
+++ extension/src/components/App/App.tsx
@@ -5,6 +5,8 @@ import { Outlet } from "react-router-dom";
 
 import { ThemeContext } from "../../context/Theme";
 import styles from "./App.module.css";
+import { Footer } from "../../layouts/Footer";
+import { Navigation } from "../../layouts/Navigation";
 
 export const App = CSSModules(
   function ({ user }: { user?: User }) {
@@ -12,7 +14,9 @@ export const App = CSSModules(
 
     return (
       <main styleName={`main main--${theme}`}>
+        <Navigation />
         <Outlet context={{ user }} />
+        <Footer />
       </main>
     );
   },
