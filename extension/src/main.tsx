cbc880f15e9bfc59f60206a8df585e421782ddee
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-02-21 19:09:18 -0500

    Wrap the entire application in 'QueryClientProvider' and use the 'ReactQueryDevtools' library.

--- extension/src/main.tsx
+++ extension/src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { Router } from "./routes/Router";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root") as HTMLDivElement).render(
   <StrictMode>
-    <MemoryRouter>
-      <Router />
-    </MemoryRouter>
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>
+        <Router />
+      </MemoryRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
