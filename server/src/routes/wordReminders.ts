9e496b9b9218fb7d755c5a55b661bb39835c0e07
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-03 02:35:43 -0500

    Add missing validation middleware for 'word_reminder' routes.

--- server/src/routes/wordReminders.ts
+++ server/src/routes/wordReminders.ts
@@ -10,7 +10,6 @@ import {
 import { validatePageQuery } from "../middleware/validate_page_query";
 import { validateSortQuery } from "../middleware/validate_sort_query";
 import {
-  validateAuto,
   validateAutoWordReminder,
   validateWordReminder,
 } from "../middleware/validate_word_reminder";
@@ -21,16 +20,11 @@ export const wordReminderRouter = Router({ caseSensitive: true });
 wordReminderRouter
   .route("/")
   .get(validatePageQuery, validateSortQuery, word_reminder_list)
-  .post(
-    validateAuto,
-    validateAutoWordReminder,
-    validateWordReminder,
-    create_word_reminder
-  )
+  .post(validateAutoWordReminder, validateWordReminder, create_word_reminder)
   .delete(delete_word_reminders);
 
 wordReminderRouter
   .route("/:wordReminderId")
   .get(validateWordReminderId, get_word_reminder)
-  .put(validateWordReminder, update_word_reminder)
-  .delete(delete_word_reminder);
+  .put(validateWordReminderId, validateWordReminder, update_word_reminder)
+  .delete(validateWordReminderId, delete_word_reminder);
