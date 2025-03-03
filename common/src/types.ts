45f963777117c6b858736769acd4ab997d99786b
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-03 03:28:38 -0500

    Add a sluew of new types for 'auto_word_reminder' rows and changing type definitions here and there.

--- common/src/types.ts
+++ common/src/types.ts
@@ -73,7 +73,6 @@ export interface ImageParams {
   word_id: string;
 }
 export interface User extends Id {
-  auto: boolean;
   email: string;
   confirmed: boolean;
   created_at: Date;
@@ -108,6 +107,15 @@ export enum Subject {
   CHANGE_VERIFICATION = "Change Account Details",
 }
 
+// Defines how often a reminder notification is sent to the user which will include all of the words in this word reminder.
+export interface AddToDate extends Id {
+  minutes: number;
+  hours: number;
+  days: number;
+  weeks: number;
+  months: number;
+}
+
 export interface WordReminder extends Id {
   created_at: Date;
   updated_at: Date;
@@ -117,12 +125,20 @@ export interface WordReminder extends Id {
   has_reminder_onload: boolean;
   // Determines if the reminder should be shown immediately upon loading the application or feature. One notification will be shown if at least one notification was emitted after the last time the user signed onto the application.
   is_active: boolean;
-  // Specifies if the reminder is active
-  reminder: AddToDate;
-  // Defines how often a reminder notification is sent to the user which will include all of the words in this word reminder.
 }
 
-export interface AddToDate {
+export interface AutoWordReminder extends Id {
+  user_id: string;
+  is_active: boolean;
+  has_learned_words: boolean;
+  has_reminder_onload: boolean;
+  order: Order;
+  word_count: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface AddToDateParams {
   minutes: number;
   hours: number;
   days: number;
@@ -131,18 +147,48 @@ export interface AddToDate {
 }
 
 interface WordReminderParams {
-  auto: boolean;
+  user_id: string;
   has_reminder_onload: boolean;
   is_active: boolean;
-  reminder: AddToDate;
+  reminder: AddToDateParams;
 }
 
 export interface WordReminderDbParams {
   user_id: string;
   has_reminder_onload: boolean;
   is_active: boolean;
-  reminder: AddToDate;
   finish: Date;
+}
+
+export interface AutoWordReminderDbParams {
+  user_id: string;
+  is_active: boolean;
+  has_reminder_onload: boolean;
+  has_learned_words: boolean;
+  order: Order;
+  word_count: number;
+}
+
+export interface AddToDatesAutoWordReminder extends Id {
+  auto_word_reminder_id: string;
+  reminder_id: string;
+  duration_id: string;
+}
+
+export interface AddToDatesAutoWordReminderDbParams {
+  auto_word_reminder_id: string;
+  reminder_id: string;
+  duration_id: string;
+}
+
+export interface AddToDatesWordReminder extends Id {
+  word_reminder_id: string;
+  reminder_id: string;
+}
+
+export interface AddToDatesWordReminderDbParams {
+  word_reminder_id: string;
+  reminder_id: string;
 }
 
 export interface ManualWordReminderParams extends WordReminderParams {
@@ -150,12 +196,14 @@ export interface ManualWordReminderParams extends WordReminderParams {
   user_words: string[];
 }
 
+// create_now
 export interface AutoWordReminderParams extends WordReminderParams {
-  create_now: boolean;
-  duration: AddToDate;
-  word_count: number;
+  user_id: string;
   has_learned_words: boolean;
   order: Order;
+  word_count: number;
+  create_now: boolean;
+  duration: AddToDateParams;
 }
 
 export interface AuthParams {
