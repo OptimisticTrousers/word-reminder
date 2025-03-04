f3c62f497b7485c2d2eba0e3589c094a6f1515c2
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-04 09:31:57 -0500

    Cleanup middleware that validates word reminder ids.

--- server/src/__tests__/validate_word_reminder_id.test.ts
+++ server/src/__tests__/validate_word_reminder_id.test.ts
@@ -1,37 +1,36 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 import request from "supertest";
 
 import { wordReminderQueries } from "../db/word_reminder_queries";
 import { validateWordReminderId } from "../middleware/validate_word_reminder_id";
 
+const message = "message";
+
+const userId = 1;
+const wordReminderId = 1;
+
+const app = express();
+app.use(express.json());
+app.delete(
+  "/api/users/:userId/wordReminders/:wordReminderId",
+  (req, res, next) => {
+    req.user = { id: userId };
+    next();
+  },
+  validateWordReminderId,
+  (req, res) => {
+    res.status(200).json({ message });
+  }
+);
+
 describe("validateWordReminderId", () => {
-  const message = "message";
-
-  const userId = "1";
-  const wordReminderId = "1";
-
-  const app = express();
-  app.use(express.json());
-  app.delete(
-    "/api/wordReminders/:wordReminderId",
-    asyncHandler(async (req, res, next) => {
-      req.user = { id: userId };
-      next();
-    }),
-    validateWordReminderId,
-    asyncHandler(async (req, res, next) => {
-      res.status(200).json({ message });
-    })
-  );
-
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it("returns a 400 status code with invalid word reminder id message", async () => {
     const response = await request(app)
-      .delete("/api/wordReminders/bob")
+      .delete(`/api/users/${userId}/wordReminders/bob`)
       .set("Accept", "application/json");
 
     expect(response.headers["content-type"]).toMatch(/json/);
@@ -44,12 +43,10 @@ describe("validateWordReminderId", () => {
   it("returns a 404 status code with word reminder not found message", async () => {
     const mockGetById = jest
       .spyOn(wordReminderQueries, "getById")
-      .mockImplementation(async () => {
-        return undefined;
-      });
+      .mockResolvedValue(undefined);
 
     const response = await request(app)
-      .delete(`/api/wordReminders/${wordReminderId}`)
+      .delete(`/api/users/${userId}/wordReminders/${wordReminderId}`)
       .set("Accept", "application/json");
 
     expect(response.headers["content-type"]).toMatch(/json/);
@@ -68,17 +65,16 @@ describe("validateWordReminderId", () => {
       created_at: new Date(),
       updated_at: new Date(),
       finish: new Date(),
+      reminder: "* * * * *",
       is_active: true,
       has_reminder_onload: true,
     };
     const mockGetById = jest
       .spyOn(wordReminderQueries, "getById")
-      .mockImplementation(async () => {
-        return wordReminder;
-      });
+      .mockResolvedValue(wordReminder);
 
     const response = await request(app)
-      .delete(`/api/wordReminders/${wordReminderId}`)
+      .delete(`/api/users/${userId}/wordReminders/${wordReminderId}`)
       .set("Accept", "application/json");
 
     expect(response.status).toBe(200);
--- server/src/middleware/validate_word_reminder_id.ts
+++ server/src/middleware/validate_word_reminder_id.ts
@@ -3,13 +3,15 @@ import asyncHandler from "express-async-handler";
 import { wordReminderQueries } from "../db/word_reminder_queries";
 
 export const validateWordReminderId = asyncHandler(async (req, res, next) => {
-  const { wordReminderId } = req.params;
-  if (isNaN(Number(wordReminderId))) {
+  const wordReminderId = Number(req.params.wordReminderId);
+
+  if (isNaN(wordReminderId)) {
     res.status(400).json({ message: "Invalid word reminder ID." });
     return;
   }
 
   const wordReminder = await wordReminderQueries.getById(wordReminderId);
+
   if (!wordReminder) {
     res.status(404).json({ message: "Word reminder not found." });
     return;
