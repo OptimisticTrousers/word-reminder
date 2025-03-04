c39405f26a29239596b4199bad76b4949aee710a
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-04 09:27:55 -0500

    Updating 'word_reminder' module that contains the 'createWordReminder' function.

--- server/src/__tests__/word_reminder.test.ts
+++ server/src/__tests__/word_reminder.test.ts
@@ -1,102 +1,58 @@
-import { addToDateQueries } from "../db/add_to_date_queries";
-import { addToDatesWordRemindersQueries } from "../db/add_to_dates_word_reminders_queries";
 import { userWordsWordRemindersQueries } from "../db/user_words_word_reminders_queries";
 import { wordReminderQueries } from "../db/word_reminder_queries";
 import { createWordReminder } from "../utils/word_reminder";
 
+const userWordIds = [1, 2];
+const wordReminderParams = {
+  user_id: 1,
+  is_active: true,
+  has_reminder_onload: true,
+  finish: new Date(),
+  reminder: "* * * * *",
+};
+const wordReminder = {
+  id: 1,
+  ...wordReminderParams,
+  created_at: new Date(),
+  updated_at: new Date(),
+};
+const userWordsWordReminders1 = {
+  id: 1,
+  word_reminder_id: wordReminder.id,
+  user_word_id: userWordIds[0],
+};
+const userWordsWordReminders2 = {
+  id: 2,
+  word_reminder_id: wordReminder.id,
+  user_word_id: userWordIds[1],
+};
+
 describe("createWordReminder", () => {
-  const reminder = {
-    minutes: 0,
-    hours: 0,
-    days: 0,
-    weeks: 0,
-    months: 0,
-  };
-  const wordReminderId = "1";
-  const reminderId = "1";
-  const wordReminder = {
-    user_id: "1",
-    is_active: true,
-    has_reminder_onload: true,
-    finish: new Date("December 17, 1995 03:24:00"),
-  };
-  const user_words = ["1", "2"];
-  const wordReminderParams = {
-    ...wordReminder,
-    reminder,
-    user_words,
-  };
-
-  const created_at = new Date("December 13, 1995 03:24:00");
-  const updated_at = new Date("December 14, 1995 03:24:00");
-
   it("calls the function to create a word reminder", async () => {
     const mockWordReminderCreate = jest
       .spyOn(wordReminderQueries, "create")
-      .mockImplementation(async () => {
-        return {
-          ...wordReminder,
-          id: wordReminderId,
-          created_at,
-          updated_at,
-        };
-      });
-    const mockAddToDateCreate = jest
-      .spyOn(addToDateQueries, "create")
-      .mockImplementation(async () => {
-        return { ...reminder, id: reminderId };
-      });
-    const mockAddToDatesWordRemindersCreate = jest
-      .spyOn(addToDatesWordRemindersQueries, "create")
-      .mockImplementation(async () => {
-        return {
-          id: "1",
-          word_reminder_id: wordReminderId,
-          reminder_id: reminderId,
-        };
-      });
+      .mockResolvedValue(wordReminder);
     const mockUserWordsWordRemindersCreate = jest
       .spyOn(userWordsWordRemindersQueries, "create")
-      .mockImplementationOnce(async () => {
-        return {
-          user_word_id: user_words[0],
-          word_reminder_id: wordReminderId,
-          id: "1",
-        };
-      })
-      .mockImplementationOnce(async () => {
-        return {
-          user_word_id: user_words[1],
-          word_reminder_id: wordReminderId,
-          id: "2",
-        };
-      });
+      .mockResolvedValueOnce(userWordsWordReminders1)
+      .mockResolvedValueOnce(userWordsWordReminders2);
 
-    const newWordReminder = await createWordReminder(wordReminderParams);
+    const newWordReminder = await createWordReminder({
+      ...wordReminderParams,
+      user_word_ids: userWordIds,
+    });
 
     expect(mockWordReminderCreate).toHaveBeenCalledTimes(1);
-    expect(mockWordReminderCreate).toHaveBeenCalledWith(wordReminder);
-    expect(mockAddToDateCreate).toHaveBeenCalledTimes(1);
-    expect(mockAddToDateCreate).toHaveBeenCalledWith(reminder);
-    expect(mockAddToDatesWordRemindersCreate).toHaveBeenCalledTimes(1);
-    expect(mockAddToDatesWordRemindersCreate).toHaveBeenCalledWith({
-      word_reminder_id: wordReminderId,
-      reminder_id: reminderId,
-    });
+    expect(mockWordReminderCreate).toHaveBeenCalledWith(wordReminderParams);
     expect(mockUserWordsWordRemindersCreate).toHaveBeenCalledTimes(2);
     expect(mockUserWordsWordRemindersCreate).toHaveBeenCalledWith({
-      user_word_id: user_words[0],
-      word_reminder_id: wordReminderId,
+      user_word_id: userWordsWordReminders1.user_word_id,
+      word_reminder_id: userWordsWordReminders1.word_reminder_id,
     });
     expect(mockUserWordsWordRemindersCreate).toHaveBeenCalledWith({
-      user_word_id: user_words[1],
-      word_reminder_id: wordReminderId,
+      user_word_id: userWordsWordReminders2.user_word_id,
+      word_reminder_id: userWordsWordReminders2.word_reminder_id,
     });
-    expect(newWordReminder).toEqual({
-      ...wordReminder,
-      id: "1",
-      created_at,
-      updated_at,
-    });
+    expect(newWordReminder).toEqual(wordReminder);
   });
 });
--- server/src/utils/word_reminder.ts
+++ server/src/utils/word_reminder.ts
@@ -1,8 +1,5 @@
-import { ManualWordReminderParams } from "common";
 import { wordReminderQueries } from "../db/word_reminder_queries";
 import { userWordsWordRemindersQueries } from "../db/user_words_word_reminders_queries";
-import { addToDatesWordRemindersQueries } from "../db/add_to_dates_word_reminders_queries";
-import { addToDateQueries } from "../db/add_to_date_queries";
 
 export const createWordReminder = async ({
   user_id,
@@ -10,23 +7,25 @@ export const createWordReminder = async ({
   has_reminder_onload,
   reminder,
   finish,
-  user_words,
-}: ManualWordReminderParams) => {
+  user_word_ids,
+}: {
+  user_id: number;
+  is_active: boolean;
+  has_reminder_onload: boolean;
+  reminder: string;
+  finish: Date;
+  user_word_ids: number[];
+}) => {
   const wordReminder = await wordReminderQueries.create({
     user_id,
     is_active: is_active,
     has_reminder_onload: has_reminder_onload,
     finish,
-  });
-  const addToDateReminder = await addToDateQueries.create(reminder);
-
-  await addToDatesWordRemindersQueries.create({
-    word_reminder_id: wordReminder.id,
-    reminder_id: addToDateReminder.id,
+    reminder,
   });
 
-  user_words.forEach(async (user_word_id: string) => {
-    await userWordsWordRemindersQueries.create({
+  user_word_ids.forEach((user_word_id: number) => {
+    userWordsWordRemindersQueries.create({
       user_word_id: user_word_id,
       word_reminder_id: wordReminder.id,
     });
