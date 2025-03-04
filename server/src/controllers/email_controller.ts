07618ecfab5a7bc4d7f8d3d066056be0cfef8ee2
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-04 09:44:35 -0500

    Refactor 'send_email' controller.

--- server/src/controllers/email_controller.ts
+++ server/src/controllers/email_controller.ts
@@ -1,20 +1,15 @@
+import { Subject, Template, Token } from "common";
 import ejs from "ejs";
 import asyncHandler from "express-async-handler";
 import { body } from "express-validator";
 import { readFile } from "node:fs/promises";
-import { SentMessageInfo } from "nodemailer";
 import path from "path";
 import { Job } from "pg-boss";
 
-import { variables } from "../config/variables";
-import { Token, tokenQueries } from "../db/token_queries";
-import { userQueries } from "../db/user_queries";
+import { boss } from "../db/boss";
+import { tokenQueries } from "../db/token_queries";
 import { errorValidationHandler } from "../middleware/error_validation_handler";
 import { email } from "../utils/email";
-import { scheduler } from "../utils/scheduler";
-
-const { FRONTEND_VERIFICATION, FRONTEND_URL, SERVER_URL, SERVER_PORT } =
-  variables;
 
 // @desc Sends an email
 // @route POST /api/emails
@@ -29,29 +24,35 @@ export const send_email = [
     .isEmail()
     .withMessage(`'email' must be a valid email.`),
   body("subject")
-    .trim()
-    .escape()
-    .isLength({ min: 1 })
-    .withMessage("'subject' must be specified."),
+    .notEmpty({ ignore_whitespace: true })
+    .withMessage("'subject' must be specified.")
+    .bail()
+    .custom((value) => Object.values<string>(Subject).includes(value))
+    .withMessage(
+      `'subject' must be a value in this enum: ${Object.values(Subject)}.`
+    ),
   body("template")
-    .trim()
-    .escape()
-    .isLength({ min: 1 })
-    .withMessage("'template' must be specified."),
+    .notEmpty({ ignore_whitespace: true })
+    .withMessage("'template' must be specified.")
+    .bail()
+    .custom((value) => Object.values<string>(Template).includes(value))
+    .withMessage(
+      `'template' must be a value in this enum: ${Object.values(Template)}.`
+    ),
   errorValidationHandler,
   asyncHandler(async (req, res) => {
-    const userId = req.params.userId;
-    const template = req.body.template;
-    const subject = req.body.subject;
+    const { userId } = req.params;
+    const { template, subject } = req.body;
 
     const emailTemplate = await readFile(
-      path.join(__dirname, "..", "views", `${template}.ejs`),
+      path.join(__dirname, "..", "views", "emails", `${template}.ejs`),
       "utf-8"
     );
     const token = await tokenQueries.create();
-    const url = `${SERVER_URL}:${SERVER_PORT}/api/users/${userId}/emails/${token.token}`;
-    const html = ejs.render(emailTemplate, { url });
-    const info: SentMessageInfo = await email.sendMail({
+    const html = ejs.render(emailTemplate, {
+      url: `/${template}/${userId}&${token.token}`,
+    });
+    const info = await email.sendMail({
       to: req.body.email,
       subject,
       html,
@@ -60,10 +61,10 @@ export const send_email = [
     const ms = 30 * 60 * 1000; // 30 minutes in ms
     const date = new Date(Date.now() + ms);
 
-    const queueName = `queue-${userId}`;
-    await scheduler.sendAfter(queueName, { token }, date);
+    const queueName = `${userId}-email-queue`;
+    await boss.sendAfter(queueName, { token }, {}, date);
 
-    await scheduler.work(queueName, async (jobs: Job<Token>[]) => {
+    await boss.work(queueName, async (jobs: Job<Token>[]) => {
       const tokens = jobs.map((job) => {
         return job.data.token;
       });
