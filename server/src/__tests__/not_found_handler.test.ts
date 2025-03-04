392dec94949d30b6babb7985592111707b65c931
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-04 09:35:41 -0500

    Cleanup middleware that validates 'NotFound' routes.

--- server/src/__tests__/not_found_handler.test.ts
+++ server/src/__tests__/not_found_handler.test.ts
@@ -4,15 +4,15 @@ import request from "supertest";
 import { errorHandler } from "../middleware/error_handler";
 import { notFoundHandler } from "../middleware/not_found_handler";
 
+const app = express();
+app.use(express.json());
+
+app.use(notFoundHandler);
+
+// Add the errorHandler middleware to catch and respond to errors
+app.use(errorHandler);
+
 describe("notFoundHandler", () => {
-  const app = express();
-  app.use(express.json());
-
-  app.use(notFoundHandler);
-
-  // Add the errorHandler middleware to catch and respond to errors
-  app.use(errorHandler);
-
   describe("Not found handler", () => {
     it("should handle undefined routes and return status 404", async () => {
       const response = await request(app).get("/unknown");
--- server/src/middleware/not_found_handler.ts
+++ server/src/middleware/not_found_handler.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 
 import { CustomNotFoundError } from "../errors/custom_not_found_error";
 
-// catch 404 and forward to error handler
 export const notFoundHandler = (
   _req: Request,
   _res: Response,
