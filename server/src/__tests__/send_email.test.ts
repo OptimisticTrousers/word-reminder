5a0aceb88b4c2a74e0ceca6644e602d6959385c3
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-04 23:56:29 -0500

    Fix 'send_email' tests.

--- server/src/__tests__/send_email.test.ts
+++ server/src/__tests__/send_email.test.ts
@@ -1,84 +1,79 @@
 import ejs from "ejs";
 import express from "express";
-import path from "path";
+import { SentMessageInfo } from "nodemailer";
 import { readFile } from "node:fs/promises";
-import request from "supertest";
+import path from "path";
 import { Job } from "pg-boss";
+import request from "supertest";
 
-import { variables } from "../config/variables";
 import { send_email } from "../controllers/email_controller";
+import { boss } from "../db/boss";
 import { tokenQueries } from "../db/token_queries";
 import { email } from "../utils/email";
-import { scheduler } from "../utils/scheduler";
+import { Subject, Template } from "common";
 
-const { SERVER_PORT, SERVER_URL } = variables;
+const userId = 1;
+const queueName = `${userId}-email-queue`;
+
+const app = express();
+app.use(express.json());
+app.post("/api/users/:userId/emails", send_email);
+
+const info: SentMessageInfo = {
+  message: "Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email",
+};
+
+const token = {
+  token: "token",
+  expires_at: new Date(),
+};
+
+const mockSend = jest.spyOn(email, "sendMail").mockResolvedValue(info);
+
+const mockCreate = jest.spyOn(tokenQueries, "create").mockResolvedValue(token);
+
+const mockSendAfter = jest
+  .spyOn(boss, "sendAfter")
+  .mockImplementation(jest.fn());
+
+let capturedCallback: any;
+
+const mockWork = jest
+  .spyOn(boss, "work")
+  .mockImplementation(async (queueName, callback) => {
+    capturedCallback = callback; // Capture the callback
+    return "";
+  });
+
+const mockDeleteAll = jest
+  .spyOn(tokenQueries, "deleteAll")
+  .mockImplementation(jest.fn());
+
+const mockJobs = [
+  { data: { token: "token1" } },
+  { data: { token: "token2" } },
+] as unknown as Job<{ token: string }>[];
+
+jest.spyOn(global.Date, "now").mockImplementation(() => {
+  return new Date(0).valueOf();
+});
+const ms = 30 * 60 * 1000; // 30 minutes in ms
 
 describe("send_email", () => {
-  const app = express();
-  app.use(express.json());
-  app.post("/api/users/:userId/emails", send_email);
-
-  const info = {
-    message:
-      "Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email",
-  };
-
-  const token = {
-    token: "token",
-    expires_at: new Date(),
-  };
-
-  const mockSend = jest
-    .spyOn(email, "sendMail")
-    .mockResolvedValue(info)
-    .mockName("sendMail");
-
-  const mockCreate = jest
-    .spyOn(tokenQueries, "create")
-    .mockResolvedValue(token)
-    .mockName("create");
-
-  const mockSendAfter = jest
-    .spyOn(scheduler, "sendAfter")
-    .mockImplementation(jest.fn())
-    .mockName("create");
-
-  let capturedCallback = async function (jobs: Job<unknown>[]) {};
-
-  const mockWork = jest
-    .spyOn(scheduler, "work")
-    .mockImplementation(async (queueName, callback) => {
-      capturedCallback = callback; // Capture the callback
-    })
-    .mockName("work");
-
-  const mockDeleteAll = jest
-    .spyOn(tokenQueries, "deleteAll")
-    .mockImplementation(jest.fn())
-    .mockName("deleteAll");
-
-  const mockJobs = [
-    { data: { token: "token1" } },
-    { data: { token: "token2" } },
-  ] as unknown as Job<{ token: string }>[];
-
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it("returns 400 status code when the email is not provided", async () => {
-    const userId = "1";
-
+  it("returns 400 status code when the 'email' is not provided", async () => {
+    const body = {
+      email: undefined,
+      template: Template.CHANGE_EMAIL,
+      subject: Subject.CHANGE_EMAIL,
+    };
     const response = await request(app)
       .post(`/api/users/${userId}/emails`)
       .set("Accept", "application/json")
-      .send({
-        email: undefined,
-        template: "template",
-        subject: "Hello ✔",
-        text: "Hello world?",
-        html: "<b>Hello world?</b>",
-      });
+      .send(body);
 
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toBe(400);
@@ -95,14 +90,11 @@ describe("send_email", () => {
     });
   });
 
-  it("returns 400 status code when the subject is not provided", async () => {
-    const userId = "1";
+  it("returns 400 status code when the 'subject' is not provided", async () => {
     const body = {
       email: "bob@protonmail.com",
-      template: "template",
+      template: Template.CHANGE_PASSWORD,
       subject: undefined,
-      text: "Hello world?",
-      html: "<b>Hello world?</b>",
     };
 
     const response = await request(app)
@@ -119,20 +111,100 @@ describe("send_email", () => {
           msg: "'subject' must be specified.",
           path: "subject",
           type: "field",
-          value: "",
+        },
+      ],
+    });
+  });
+
+  it("returns 400 status code when the 'template' is not provided", async () => {
+    const body = {
+      email: "bob@protonmail.com",
+      template: undefined,
+      subject: Subject.CHANGE_PASSWORD,
+    };
+
+    const response = await request(app)
+      .post(`/api/users/${userId}/emails`)
+      .set("Accept", "application/json")
+      .send(body);
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      errors: [
+        {
+          location: "body",
+          msg: "'template' must be specified.",
+          path: "template",
+          type: "field",
+        },
+      ],
+    });
+  });
+
+  it("returns 400 status code when the 'template' is not in the 'Template' enum", async () => {
+    const body = {
+      email: "bob@protonmail.com",
+      template: "invalid-template",
+      subject: Subject.CHANGE_PASSWORD,
+    };
+
+    const response = await request(app)
+      .post(`/api/users/${userId}/emails`)
+      .set("Accept", "application/json")
+      .send(body);
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      errors: [
+        {
+          location: "body",
+          msg: `'template' must be a value in this enum: ${Object.values(
+            Template
+          )}.`,
+          path: "template",
+          type: "field",
+          value: body.template,
+        },
+      ],
+    });
+  });
+
+  it("returns 400 status code when the 'subject' is not in the Subject' enum", async () => {
+    const body = {
+      email: "bob@protonmail.com",
+      template: Template.FORGOT_PASSWORD,
+      subject: "invalid-subject",
+    };
+
+    const response = await request(app)
+      .post(`/api/users/${userId}/emails`)
+      .set("Accept", "application/json")
+      .send(body);
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      errors: [
+        {
+          location: "body",
+          msg: `'subject' must be a value in this enum: ${Object.values(
+            Subject
+          )}.`,
+          path: "subject",
+          type: "field",
+          value: body.subject,
         },
       ],
     });
   });
 
   it("returns 400 status code when the email is not a valid email", async () => {
-    const userId = "1";
     const body = {
       email: "email",
-      template: "template",
-      subject: "Hello ✔",
-      text: "Hello world?",
-      html: "<b>Hello world?</b>",
+      template: Template.CONFIRM_ACCOUNT,
+      subject: Subject.CONFIRM_ACCOUNT,
     };
 
     const response = await request(app)
@@ -155,166 +227,175 @@ describe("send_email", () => {
     });
   });
 
-  describe("template", () => {
-    it("returns 400 status code when the template is not provided", async () => {
-      const userId = "1";
+  it("calls the functions to send email with 'confirm_account' template", async () => {
+    const body = {
+      email: "bob@protonmail.com",
+      template: Template.CONFIRM_ACCOUNT,
+      subject: Subject.CONFIRM_ACCOUNT,
+    };
+    const emailTemplate = await readFile(
+      path.join(__dirname, "..", "views", "emails", `${body.template}.ejs`),
+      "utf-8"
+    );
 
-      const response = await request(app)
-        .post(`/api/users/${userId}/emails`)
-        .set("Accept", "application/json")
-        .send({
-          email: "bob@protonmail.com",
-          template: undefined,
-          subject: "Hello ✔",
-          text: "Hello world?",
-          html: "<b>Hello world?</b>",
-        });
+    const response = await request(app)
+      .post(`/api/users/${userId}/emails`)
+      .set("Accept", "application/json")
+      .send(body);
+    await capturedCallback(mockJobs);
 
-      expect(response.headers["content-type"]).toMatch(/json/);
-      expect(response.status).toBe(400);
-      expect(response.body).toEqual({
-        errors: [
-          {
-            location: "body",
-            msg: "'template' must be specified.",
-            path: "template",
-            type: "field",
-            value: "",
-          },
-        ],
-      });
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ info });
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(mockCreate).toHaveBeenCalledWith();
+    expect(mockSend).toHaveBeenCalledTimes(1);
+    expect(mockSend).toHaveBeenCalledWith({
+      html: ejs.render(emailTemplate, {
+        url: `/${body.template}/${userId}&${token.token}`,
+      }),
+      subject: body.subject,
+      to: body.email,
     });
+    expect(mockSendAfter).toHaveBeenCalledTimes(1);
+    expect(mockSendAfter).toHaveBeenCalledWith(
+      queueName,
+      { token },
+      {},
+      new Date(ms)
+    );
+    expect(mockWork).toHaveBeenCalledTimes(1);
+    expect(mockWork).toHaveBeenCalledWith(queueName, capturedCallback);
+    expect(mockDeleteAll).toHaveBeenCalledTimes(1);
+    expect(mockDeleteAll).toHaveBeenCalledWith(["token1", "token2"]);
+  });
 
-    it("calls the functions to send email with 'email_verification' template", async () => {
-      const body = {
-        email: "bob@protonmail.com",
-        template: "email_verification",
-        subject: "Verify Your Email",
-      };
-      const userId = "1";
-      const queueName = `queue-${userId}`;
-      const emailTemplate = await readFile(
-        path.join(__dirname, "..", "views", `${body.template}.ejs`),
-        "utf-8"
-      );
+  it("calls the functions to send email with 'change_password' template", async () => {
+    const body = {
+      email: "bob@protonmail.com",
+      template: Template.CHANGE_PASSWORD,
+      subject: Subject.CHANGE_PASSWORD,
+    };
+    const emailTemplate = await readFile(
+      path.join(__dirname, "..", "views", "emails", `${body.template}.ejs`),
+      "utf-8"
+    );
 
-      const response = await request(app)
-        .post(`/api/users/${userId}/emails`)
-        .set("Accept", "application/json")
-        .send({ ...body });
-      await capturedCallback(mockJobs);
+    const response = await request(app)
+      .post(`/api/users/${userId}/emails`)
+      .set("Accept", "application/json")
+      .send(body);
+    await capturedCallback(mockJobs);
 
-      expect(response.headers["content-type"]).toMatch(/json/);
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({ info });
-      expect(mockCreate).toHaveBeenCalledTimes(1);
-      expect(mockCreate).toHaveBeenCalledWith();
-      expect(mockSend).toHaveBeenCalledTimes(1);
-      expect(mockSend).toHaveBeenCalledWith({
-        html: ejs.render(emailTemplate, {
-          url: `${SERVER_URL}:${SERVER_PORT}/api/users/${userId}/emails/${token.token}`,
-        }),
-        subject: body.subject,
-        to: body.email,
-      });
-      expect(mockSendAfter).toHaveBeenCalledTimes(1);
-      expect(mockSendAfter).toHaveBeenCalledWith(
-        queueName,
-        { token },
-        expect.any(Date)
-      );
-      expect(mockWork).toHaveBeenCalledTimes(1);
-      expect(mockWork).toHaveBeenCalledWith(queueName, expect.any(Function));
-      expect(mockDeleteAll).toHaveBeenCalledTimes(1);
-      expect(mockDeleteAll).toHaveBeenCalledWith(["token1", "token2"]);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ info });
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(mockCreate).toHaveBeenCalledWith();
+    expect(mockSend).toHaveBeenCalledTimes(1);
+    expect(mockSend).toHaveBeenCalledWith({
+      html: ejs.render(emailTemplate, {
+        url: `/${body.template}/${userId}&${token.token}`,
+      }),
+      subject: body.subject,
+      to: body.email,
     });
+    expect(mockSendAfter).toHaveBeenCalledTimes(1);
+    expect(mockSendAfter).toHaveBeenCalledWith(
+      queueName,
+      { token },
+      {},
+      new Date(ms)
+    );
+    expect(mockWork).toHaveBeenCalledTimes(1);
+    expect(mockWork).toHaveBeenCalledWith(queueName, capturedCallback);
+    expect(mockDeleteAll).toHaveBeenCalledTimes(1);
+    expect(mockDeleteAll).toHaveBeenCalledWith(["token1", "token2"]);
+  });
 
-    it("calls the functions to send email with 'change_email_verification' template", async () => {
-      const body = {
-        email: "bob@protonmail.com",
-        template: "change_email_verification",
-        subject: "Change Email",
-      };
-      const userId = "1";
-      const queueName = `queue-${userId}`;
-      const emailTemplate = await readFile(
-        path.join(__dirname, "..", "views", `${body.template}.ejs`),
-        "utf-8"
-      );
+  it("calls the functions to send email with 'change_email' template", async () => {
+    const body = {
+      email: "bob@protonmail.com",
+      template: Template.CHANGE_EMAIL,
+      subject: Subject.CHANGE_EMAIL,
+    };
+    const emailTemplate = await readFile(
+      path.join(__dirname, "..", "views", "emails", `${body.template}.ejs`),
+      "utf-8"
+    );
 
-      const response = await request(app)
-        .post(`/api/users/${userId}/emails`)
-        .set("Accept", "application/json")
-        .send(body);
-      await capturedCallback(mockJobs);
+    const response = await request(app)
+      .post(`/api/users/${userId}/emails`)
+      .set("Accept", "application/json")
+      .send(body);
+    await capturedCallback(mockJobs);
 
-      expect(response.headers["content-type"]).toMatch(/json/);
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({ info });
-      expect(mockCreate).toHaveBeenCalledTimes(1);
-      expect(mockCreate).toHaveBeenCalledWith();
-      expect(mockSend).toHaveBeenCalledTimes(1);
-      expect(mockSend).toHaveBeenCalledWith({
-        html: ejs.render(emailTemplate, {
-          url: `${SERVER_URL}:${SERVER_PORT}/api/users/${userId}/emails/${token.token}`,
-        }),
-        subject: body.subject,
-        to: body.email,
-      });
-      expect(mockSendAfter).toHaveBeenCalledTimes(1);
-      expect(mockSendAfter).toHaveBeenCalledWith(
-        queueName,
-        { token },
-        expect.any(Date)
-      );
-      expect(mockWork).toHaveBeenCalledTimes(1);
-      expect(mockWork).toHaveBeenCalledWith(queueName, expect.any(Function));
-      expect(mockDeleteAll).toHaveBeenCalledTimes(1);
-      expect(mockDeleteAll).toHaveBeenCalledWith(["token1", "token2"]);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ info });
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(mockCreate).toHaveBeenCalledWith();
+    expect(mockSend).toHaveBeenCalledTimes(1);
+    expect(mockSend).toHaveBeenCalledWith({
+      html: ejs.render(emailTemplate, {
+        url: `/${body.template}/${userId}&${token.token}`,
+      }),
+      subject: body.subject,
+      to: body.email,
     });
+    expect(mockSendAfter).toHaveBeenCalledTimes(1);
+    expect(mockSendAfter).toHaveBeenCalledWith(
+      queueName,
+      { token },
+      {},
+      new Date(ms)
+    );
+    expect(mockWork).toHaveBeenCalledTimes(1);
+    expect(mockWork).toHaveBeenCalledWith(queueName, capturedCallback);
+    expect(mockDeleteAll).toHaveBeenCalledTimes(1);
+    expect(mockDeleteAll).toHaveBeenCalledWith(["token1", "token2"]);
+  });
 
-    it("calls the functions to send email with 'change_password_verification' template", async () => {
-      const body = {
-        email: "bob@protonmail.com",
-        template: "change_password_verification",
-        subject: "Change Password",
-      };
-      const userId = "1";
-      const queueName = `queue-${userId}`;
-      const emailTemplate = await readFile(
-        path.join(__dirname, "..", "views", `${body.template}.ejs`),
-        "utf-8"
-      );
+  it("calls the functions to send email with 'forgot_password' template", async () => {
+    const body = {
+      email: "bob@protonmail.com",
+      template: Template.FORGOT_PASSWORD,
+      subject: Subject.FORGOT_PASSWORD,
+    };
+    const emailTemplate = await readFile(
+      path.join(__dirname, "..", "views", "emails", `${body.template}.ejs`),
+      "utf-8"
+    );
 
-      const response = await request(app)
-        .post(`/api/users/${userId}/emails`)
-        .set("Accept", "application/json")
-        .send(body);
-      await capturedCallback(mockJobs);
+    const response = await request(app)
+      .post(`/api/users/${userId}/emails`)
+      .set("Accept", "application/json")
+      .send(body);
+    await capturedCallback(mockJobs);
 
-      expect(response.headers["content-type"]).toMatch(/json/);
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({ info });
-      expect(mockCreate).toHaveBeenCalledTimes(1);
-      expect(mockCreate).toHaveBeenCalledWith();
-      expect(mockSend).toHaveBeenCalledTimes(1);
-      expect(mockSend).toHaveBeenCalledWith({
-        html: ejs.render(emailTemplate, {
-          url: `${SERVER_URL}:${SERVER_PORT}/api/users/${userId}/emails/${token.token}`,
-        }),
-        subject: body.subject,
-        to: body.email,
-      });
-      expect(mockSendAfter).toHaveBeenCalledTimes(1);
-      expect(mockSendAfter).toHaveBeenCalledWith(
-        queueName,
-        { token },
-        expect.any(Date)
-      );
-      expect(mockWork).toHaveBeenCalledTimes(1);
-      expect(mockWork).toHaveBeenCalledWith(queueName, expect.any(Function));
-      expect(mockDeleteAll).toHaveBeenCalledTimes(1);
-      expect(mockDeleteAll).toHaveBeenCalledWith(["token1", "token2"]);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ info });
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+    expect(mockCreate).toHaveBeenCalledWith();
+    expect(mockSend).toHaveBeenCalledTimes(1);
+    expect(mockSend).toHaveBeenCalledWith({
+      html: ejs.render(emailTemplate, {
+        url: `/${body.template}/${userId}&${token.token}`,
+      }),
+      subject: body.subject,
+      to: body.email,
     });
+    expect(mockSendAfter).toHaveBeenCalledTimes(1);
+    expect(mockSendAfter).toHaveBeenCalledWith(
+      queueName,
+      { token },
+      {},
+      new Date(ms)
+    );
+    expect(mockWork).toHaveBeenCalledTimes(1);
+    expect(mockWork).toHaveBeenCalledWith(queueName, capturedCallback);
+    expect(mockDeleteAll).toHaveBeenCalledTimes(1);
+    expect(mockDeleteAll).toHaveBeenCalledWith(["token1", "token2"]);
   });
 });
