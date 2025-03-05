0af27f8fe3d1ccf3447208adc24c9c4cb6048f10
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-04 23:59:03 -0500

    Only use prod database for 'pg-boss'. Delete unneeded queries.

--- server/src/db/boss.ts
+++ server/src/db/boss.ts
@@ -2,10 +2,8 @@ import PgBoss from "pg-boss";
 
 import { variables } from "../config/variables";
 
-const { DATABASE_URL, NODE_ENV, TEST_DATABASE_URL } = variables;
+const { DATABASE_URL } = variables;
 
-export const boss = new PgBoss(
-  NODE_ENV === "test" ? TEST_DATABASE_URL : DATABASE_URL
-);
+export const boss = new PgBoss(DATABASE_URL);
 
 boss.on("error", console.error);
