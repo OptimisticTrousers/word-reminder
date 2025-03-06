72cb3fde4a5fd0208fc5a075c32c8595c228046c
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-05 23:04:56 -0500

    Add a date module to convert to and from an object of minutes, hours, days, and weeks to milliseconds.

--- extension/src/utils/date/date.test.ts
+++ extension/src/utils/date/date.test.ts
@@ -1,65 +1,115 @@
-import { addToDateToString } from "./date";
+import { msToUnits, unitsToMs } from "./date";
 
-describe("addToDateToString", () => {
-  it("returns an empty interval when addToDate object is filled with zeroes", async () => {
-    const addToDate = {
-      minutes: 0,
-      hours: 0,
-      days: 0,
-      weeks: 0,
-      months: 0,
-    };
-    const interval = addToDateToString(addToDate);
-    expect(interval).toBe("");
+describe("date", () => {
+  describe("msToUnits", () => {
+    it("converts ms to units", () => {
+      const units = msToUnits(3510231);
+      expect(units).toEqual({
+        days: 0,
+        hours: 0,
+        minutes: 58,
+        weeks: 0,
+      });
+    });
+
+    it("returns empty object when ms is a negative number", () => {
+      const units = msToUnits(-1);
+      expect(units).toEqual({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        weeks: 0,
+      });
+    });
   });
 
-  it("returns an interval when all properties of addToDate is singular", async () => {
-    const addToDate = {
-      minutes: 1,
-      hours: 1,
-      days: 1,
-      weeks: 1,
-      months: 1,
-    };
-    const interval = addToDateToString(addToDate);
-    expect(interval).toBe("every 1 month, 1 week, 1 day, 1 hour, and 1 minute");
-  });
+  describe("unitsToMs", () => {
+    it("converts units to ms", () => {
+      const ms = unitsToMs({
+        minutes: 14,
+        hours: 12,
+        days: 8,
+        weeks: 2,
+      });
 
-  it("returns an interval when one property of addToDate is plural", async () => {
-    const addToDate = {
-      minutes: 0,
-      hours: 2,
-      days: 0,
-      weeks: 0,
-      months: 0,
-    };
-    const interval = addToDateToString(addToDate);
-    expect(interval).toBe("every 2 hours");
-  });
+      const minutes = 1000 * 60 * 14; // (14 min * 60 sec/1 min * 1000 ms/1 sec)
+      const hours = 1000 * 60 * 60 * 12; // (12 hr * 60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+      const days = 1000 * 60 * 60 * 24 * 8; // (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+      const weeks = 1000 * 60 * 60 * 24 * 7 * 2; // (2 weeks * 7 days/1 week * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+      const time = minutes + hours + days + weeks;
+      expect(ms).toBe(time);
+    });
 
-  it("returns an interval when two property of addToDate are not zeroes", async () => {
-    const addToDate = {
-      minutes: 30,
-      hours: 1,
-      days: 0,
-      weeks: 0,
-      months: 0,
-    };
-    const interval = addToDateToString(addToDate);
-    expect(interval).toBe("every 1 hour and 30 minutes");
-  });
+    it("counts minutes as 0 when minutes is a negative number", () => {
+      const ms = unitsToMs({
+        minutes: -1,
+        hours: 12,
+        days: 8,
+        weeks: 2,
+      });
 
-  it("returns an interval when all properties of addToDate are not zeroes", async () => {
-    const addToDate = {
-      minutes: 30,
-      hours: 1,
-      days: 2,
-      weeks: 3,
-      months: 5,
-    };
-    const interval = addToDateToString(addToDate);
-    expect(interval).toBe(
-      "every 5 months, 3 weeks, 2 days, 1 hour, and 30 minutes"
-    );
+      expect(ms).toEqual(
+        unitsToMs({
+          minutes: 0,
+          hours: 12,
+          days: 8,
+          weeks: 2,
+        })
+      );
+    });
+
+    it("counts hours as 0 when hours is a negative number", () => {
+      const units = unitsToMs({
+        minutes: 14,
+        hours: -1,
+        days: 8,
+        weeks: 2,
+      });
+
+      expect(units).toEqual(
+        unitsToMs({
+          minutes: 14,
+          hours: 0,
+          days: 8,
+          weeks: 2,
+        })
+      );
+    });
+
+    it("counts days as 0 when days is a negative number", () => {
+      const units = unitsToMs({
+        minutes: 14,
+        hours: 12,
+        days: -1,
+        weeks: 2,
+      });
+
+      expect(units).toEqual(
+        unitsToMs({
+          minutes: 14,
+          hours: 12,
+          days: 0,
+          weeks: 2,
+        })
+      );
+    });
+
+    it("counts weeks as 0 when weeks is a negative number", () => {
+      const units = unitsToMs({
+        minutes: 14,
+        hours: 12,
+        days: 8,
+        weeks: -1,
+      });
+
+      expect(units).toEqual(
+        unitsToMs({
+          minutes: 14,
+          hours: 12,
+          days: 8,
+          weeks: 0,
+        })
+      );
+    });
   });
 });
--- extension/src/utils/date/date.ts
+++ extension/src/utils/date/date.ts
@@ -1,36 +1,57 @@
-import { AddToDateParams } from "common";
-
-export function addToDateToString(addToDate: AddToDateParams) {
-  const intervals = [];
-
-  if (addToDate.months) {
-    intervals.push(
-      `${addToDate.months} month${addToDate.months > 1 ? "s" : ""}`
-    );
-  }
-  if (addToDate.weeks) {
-    intervals.push(`${addToDate.weeks} week${addToDate.weeks > 1 ? "s" : ""}`);
-  }
-  if (addToDate.days) {
-    intervals.push(`${addToDate.days} day${addToDate.days > 1 ? "s" : ""}`);
-  }
-  if (addToDate.hours) {
-    intervals.push(`${addToDate.hours} hour${addToDate.hours > 1 ? "s" : ""}`);
-  }
-  if (addToDate.minutes) {
-    intervals.push(
-      `${addToDate.minutes} minute${addToDate.minutes > 1 ? "s" : ""}`
-    );
+export function msToUnits(ms: number) {
+  if (ms < 0) {
+    return { minutes: 0, hours: 0, days: 0, weeks: 0 };
   }
 
-  const formatter = new Intl.ListFormat("en", {
-    style: "long",
-    type: "conjunction",
-  });
+  const minuteMs = 1000 * 60; // (60 sec/1 min * 1000 ms/1 sec)
+  const hourMs = minuteMs * 60; // (60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+  const dayMs = hourMs * 24; // (24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+  const weekMs = dayMs * 7; // (7 days/1 week * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
 
-  const string = formatter.format(intervals);
-  if (string === "") {
-    return "";
+  const weeks = Math.floor(ms / weekMs);
+  ms %= weekMs;
+
+  const days = Math.floor(ms / dayMs);
+  ms %= dayMs;
+
+  const hours = Math.floor(ms / hourMs);
+  ms %= hourMs;
+
+  const minutes = Math.floor(ms / minuteMs);
+
+  return {
+    minutes,
+    hours,
+    days,
+    weeks,
+  };
+}
+
+export function unitsToMs(units: {
+  minutes: number;
+  hours: number;
+  days: number;
+  weeks: number;
+}) {
+  const minuteMs = 60 * 1000; // (60 sec/1 min * 1000 ms/1 sec)
+  const hourMs = 60 * minuteMs; // (60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+  const dayMs = 24 * hourMs; // (24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+  const weekMs = 7 * dayMs; // (7 days/1 week * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms/1 sec)
+
+  let totalMs = 0;
+
+  if (units.minutes >= 0) {
+    totalMs += units.minutes * minuteMs;
   }
-  return `every ${string}`;
+  if (units.hours >= 0) {
+    totalMs += units.hours * hourMs;
+  }
+  if (units.days >= 0) {
+    totalMs += units.days * dayMs;
+  }
+  if (units.weeks >= 0) {
+    totalMs += units.weeks * weekMs;
+  }
+
+  return totalMs;
 }
