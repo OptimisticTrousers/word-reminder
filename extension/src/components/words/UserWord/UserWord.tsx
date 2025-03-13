b4e13f54a3f55e3f8f067deb5d34a8049243d281
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-12 22:25:08 -0400

    Remove unnecessary headings in 'UserWords' and wrapping 'CondensedWord' in a div.

--- extension/src/components/words/UserWord/UserWord.tsx
+++ extension/src/components/words/UserWord/UserWord.tsx
@@ -47,29 +47,25 @@ export const UserWord = CSSModules(
             <p styleName="word__name">Learned: {learned}</p>
             <div styleName="word__divider"></div>
             <div styleName="word__buttons">
-              <h3 styleName="word__heading">
-                <button
-                  id="accordion-button"
-                  type="button"
-                  styleName="word__button word__button--info"
-                  onClick={toggleAccordion}
-                  aria-expanded={isAccordionOpen}
-                  aria-label="More word details"
-                  aria-controls="accordion"
-                >
-                  <Info styleName="word__icon" />
-                </button>
-              </h3>
-              <h3 styleName="word__heading">
-                <button
-                  styleName="word__delete word__button--delete"
-                  onClick={toggleModal}
-                  aria-haspopup={true}
-                  aria-label="Open delete user word modal"
-                >
-                  <Trash styleName="word__icon" />
-                </button>
-              </h3>
+              <button
+                id="accordion-button"
+                type="button"
+                styleName="word__button word__button--info"
+                onClick={toggleAccordion}
+                aria-expanded={isAccordionOpen}
+                aria-label="More word details"
+                aria-controls="accordion"
+              >
+                <Info styleName="word__icon" />
+              </button>
+              <button
+                styleName="word__delete word__button--delete"
+                onClick={toggleModal}
+                aria-haspopup={true}
+                aria-label="Open delete user word modal"
+              >
+                <Trash styleName="word__icon" />
+              </button>
               <Link to={`/userWords/${id}`}>More Word Details</Link>
             </div>
           </article>
