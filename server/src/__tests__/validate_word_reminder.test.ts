c4547addf771dbc2780d773aa9c913b82bf68955
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-04-09 15:17:35 -0400

    Changing word reminder create and update modals to expect user words from the backend side for convenience sake.

--- server/src/__tests__/validate_word_reminder.test.ts
+++ server/src/__tests__/validate_word_reminder.test.ts
@@ -309,7 +309,7 @@ describe("validateWordReminder", () => {
         );
         const body = {
           finish: undefined,
-          user_word_ids: [userWord1.id, userWord2.id, userWord3.id],
+          user_words: [userWord1, userWord2, userWord3],
         };
 
         const response = await request(app)
@@ -344,7 +344,7 @@ describe("validateWordReminder", () => {
         );
         const body = {
           finish: "string",
-          user_word_ids: [userWord1.id, userWord2.id, userWord3.id],
+          user_words: [userWord1, userWord2, userWord3],
         };
 
         const response = await request(app)
@@ -381,7 +381,7 @@ describe("validateWordReminder", () => {
         const finish = new Date(1);
         const body = {
           finish,
-          user_word_ids: [userWord1.id, userWord2.id, userWord3.id],
+          user_words: [userWord1, userWord2, userWord3],
         };
 
         const response = await request(app)
@@ -433,8 +433,8 @@ describe("validateWordReminder", () => {
           errors: [
             {
               location: "body",
-              msg: "'user_word_ids' must be specified.",
-              path: "user_word_ids",
+              msg: "'user_words' must be specified.",
+              path: "user_words",
               type: "field",
             },
           ],
@@ -454,7 +454,7 @@ describe("validateWordReminder", () => {
         );
         const body = {
           finish: new Date(Date.now() + 1000),
-          user_word_ids: "string",
+          user_words: "string",
         };
 
         const response = await request(app)
@@ -468,8 +468,8 @@ describe("validateWordReminder", () => {
           errors: [
             {
               location: "body",
-              msg: "'user_word_ids' must be an array.",
-              path: "user_word_ids",
+              msg: "'user_words' must be an array.",
+              path: "user_words",
               type: "field",
               value: "string",
             },
@@ -491,7 +491,7 @@ describe("validateWordReminder", () => {
       );
       const body = {
         finish: new Date(Date.now() + 1000),
-        user_word_ids: [1, 2],
+        user_words: [1, 2],
       };
 
       const response = await request(app)
